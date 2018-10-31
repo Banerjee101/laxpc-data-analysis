1952614ea44231835863576b8aae02e86ae3f199
Ankush Banerjee <43135608+Banerjee101@users.noreply.github.com>
2018-11-01 02:57:17 +0530

    Update laxpcl_soft.py
    Made some minor changes again to the code such as some minor restructuring so that it becomes easy to read and understand.

--- laxpcl_soft.py
+++ laxpcl_soft.py
@@ -1,9 +1,9 @@
-#############################################################################################################################################################
+###################################################################################################################################################################################################################################################################################################################################
 #This code was originally developed by Vikas Chand (TIFR) during his PhD Thesis work. 
 #My work on this code was to improve on it as I see Fit
 #If you want to edit/improve it, please leave this section intact
 #also, for any suggestions, email ankushabanerjee@gmail.com
-#############################################################################################################################################################
+###################################################################################################################################################################################################################################################################################################################################
 from __future__ import division
 import os
 import numpy as np
@@ -14,7 +14,7 @@ from astropy.io import fits
 from pylab import *
 from astropy.io.fits import update
 
-########################################################################################################################################################################################################################
+###################################################################################################################################################################################################################################################################################################################################
 
 def LAXE(E_start, E_stop, E_rsp1):
 	for i in range(len(E_rsp1)):
@@ -40,10 +40,10 @@ def pcu_chan_bin(pcu_no, ch_min, ch_max):
 		N_Chns = 256
     	return ch_min_binned,ch_max_binned,N_Chns
 
-########################################################################################################################################################################################################################
+###################################################################################################################################################################################################################################################################################################################################
 
-pcu_no = 2					#PCU you need
-time_bin = 1					#Time Bin in seconds
+pcu_no = 1					#PCU you need
+time_bin = 16					#Time Bin in seconds
 anod = 1					#Anode selection, refer the laxpc readme file (ian)
 
 date_obs_yy = 17				#date of observation, change this as required
@@ -51,14 +51,21 @@ date_obs_mm = 4
 date_obs_dd = 8
 
 minimum_energy = 3.0				#energy range selecton. Need to be float
-maximum_energy = 5.0
+maximum_energy = 21.0				#For HID and/or CCD remember to use 1:2 2:4 ratio for the energies of soft and hard colors (3-5 5-9 9-13 13-21 3-9 9-21 3-21)
+						#Also, when plotting the HID, you need to take the intensity of the LC with the energy ranges spanning the extreme ends of the HID energies that you are considering
+						#meaning, when plotting HID of 3-5 and 5-9 you take the intensity axis from 3-9
 
 uld_bin = -1 					#change as required, ref the laxpc readme file for reference
 Erth_occ = 1 
 evt_flg = 2
 
+directory_source_loc = "/home/ankush/Desktop/Workspace_project/1A_2466-588/LAXPC/outputdata/laxpcsoftv2.5_6Aug2018/"		#Change this as needed. This is where your data would be generated and later stored
+directory_dest_loc = "./Productions/"
+
 rmf_list_lxp1 = "lx10cshm07v1.0_t1.txt"					#change this each time u use a new observation/response. You would have to open the respons file and save it as an ascii table using fv (heasoft)
 rmf_list_lxp2 = "lx20cshp05v1.0_t1.txt"
+
+###################################################################################################################################################################################################################################################################################################################################
 
 if pcu_no == 1:
 	rsp_file = ascii.read(rmf_list_lxp1, header_start = None , data_start = 0)
@@ -72,8 +79,6 @@ else:
 Ersp1 = rsp_file['col2']
 Ersp2 = rsp_file['col3']
 
-########################################################################################################################################################################################################################
-
 ch_min, ch_max, E_min, E_max = LAXE(minimum_energy, maximum_energy,  Ersp1)
 print ch_min, ch_max, E_min, E_max
 
@@ -81,11 +86,11 @@ ch_min_binned,ch_max_binned,ch_nos = pcu_chan_bin(pcu_no, ch_min, ch_max)
 
 ascii.write([[ch_min_binned], [ch_max_binned],[E_min], [E_max]], "Selected_channels_for_this_run.txt", overwrite=True)
 
-########################################################################################################################################################################################################################
+###################################################################################################################################################################################################################################################################################################################################
  
 #This is where the magic happens, the code calls in laxpc software and passes the necessary arguments to it. The same is done with backshiftv2 and lcmath to generate the shifted bg and subrtact from the lightcurve
 
-#One problem with this section is lcmath gives asynchronous warning, any suggestions are welcomed
+#One problem with this section is lcmath gives asynchronous warning for 1 second tbin, this does not happen with other time bins
 
 lx_cmd = "printf \""+str(pcu_no)+" "+str(time_bin)+" "+str(anod)+"\n"+str(ch_min_binned)+" "+str(ch_max_binned)+"\n"+str(ch_nos)+"\n"+str(uld_bin)+" "+str(Erth_occ)+" "+ str(evt_flg)+"\n\" | ./laxpcl1"
 #print lx_cmd
@@ -100,10 +105,8 @@ lcmath_cmd = "printf \""+"lxp"+str(pcu_no)+"level2.lcurv"+"\n"+"lxp"+str(pcu_no)
 os.system(lcmath_cmd)
 
 
-########################################################################################################################################################################################################################
+###################################################################################################################################################################################################################################################################################################################################
 #This section moves all the essential files to another directory
-directory_source_loc = "/home/ankush/Desktop/Workspace_project/1A_2466-588/LAXPC/outputdata/laxpcsoftv2.5_6Aug2018/"
-directory_dest_loc = "./Productions/"
 
 lc_move = "mv "+directory_source_loc+"lxp"+str(pcu_no)+"level2.lcurv"+" "+directory_dest_loc+"lxp"+str(pcu_no)+"_"+str(anod)+"level2_"+str(time_bin)+"s_"+str(int(round(E_min)))+"_"+str(int(round(E_max)))+".lcurv"
 os.system(lc_move)
@@ -134,7 +137,7 @@ os.system(corr_lcsr_move)
 shift_backsub_lc_move = "mv "+directory_source_loc+"level2_"+str(pcu_no)+"_"+str(time_bin)+"s_"+str(int(E_min))+"_"+str(int(E_max))+"_shifted_backsub.lcurv"+" "+directory_dest_loc+"lxp"+str(pcu_no)+"_"+str(anod)+"level2_"+str(time_bin)+"s_"+str(int(round(E_min)))+"_"+str(int(round(E_max)))+"_shifted_backsub.lcurv"
 os.system(shift_backsub_lc_move)
 
-########################################################################################################################################################################################################################
+###################################################################################################################################################################################################################################################################################################################################
 
 
 
